9fbc8a2bc9c42b62ced942f71377ec1de9694706
Joel <jmjoelmor891@gmail.com>
2022-01-27 16:55:33 -0600

    Prueba 1

---
+++ restapi/PruebaApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PruebaapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PruebaApi'
---
+++ restapi/PruebaApi/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from django.db.models import fields
+from django.db.models.base import Model
+from rest_framework import serializers
+from .models import Persona
+
+class PersonaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Persona
+        fields='__all__'
